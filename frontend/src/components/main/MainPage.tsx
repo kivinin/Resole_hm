dc08cc3d89f0512021379473a0db166799ab8955
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-04-21 15:50:58 +0300

    add navbar

---
+++ frontend/src/components/main/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage(): JSX.Element {
+  return (
+    <div>
+      <h1 style={{ color: 'white' }}>Main Page</h1>
+    </div>
+  );
+}
+
+export default MainPage;
