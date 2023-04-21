9e275f0dc01e751e203eacdc206ccb6a2d477b66
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-04-21 17:44:39 +0300

    add YandexMap

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -4,14 +4,15 @@ import MainPage from '../components/main/MainPage';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import SliderPhoto from '../components/features/SliderPhoto/SliderPhoto';
+import Map from '../components/map/MapYandex';
 
 function App():JSX.Element {
   return (
     <div className="App">
         <Navbar/>
       <MainPage/>
-      <Footer/>
       <SliderPhoto />
+      <Footer/>
     </div>
   );
 }
--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import { MDBFooter } from 'mdb-react-ui-kit';
+import React from "react";
+import { MDBFooter } from "mdb-react-ui-kit";
+import MapYandex from "../map/MapYandex";
 
 function Footer() {
-    return (
-        <MDBFooter bgColor='light' className='text-center text-lg-left footer'>
-        <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          &copy; {new Date().getFullYear()} Copyright:{' '}
-          <a className='text-dark' href='https://mdbootstrap.com/'>
-            MDBootstrap.com
-          </a>
+  return (
+    <MDBFooter bgColor="light" className="text-center text-lg-left footer">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        <div
+        >
+          <MapYandex />
         </div>
-      </MDBFooter>
-    );
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
 
 export default Footer;
