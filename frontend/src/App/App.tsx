7654eabad4a6f2a264232533ce9dc80e935a3b0a
Ivan <ivanpakhtusov@gmail.com>
2023-04-23 15:05:57 +0500

    changes

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,20 +1,12 @@
-
 import NotFound from "../components/404/NotFound";
 import { Route, Routes } from "react-router";
-import React, { useEffect } from 'react';
-import './App.css';
-import MainPage from '../components/main/MainPage';
-import Navbar from '../components/navbar/Navbar';
-import Footer from '../components/footer/Footer';
-import SliderPhoto from '../components/features/SliderPhoto/SliderPhoto';
-import Video from '../components/features/Video/Video';
-import ModalOrder from '../components/Modal/ModalOrder';
-import { getService } from '../json/jsonSlice';
-import { useAppDispatch } from '../store';
-import ServiceList from '../components/services/ServiceList';
-import Login from '../components/Auth/Login';
-
-
+import React, { useEffect } from "react";
+import "./App.css";
+import MainPage from "../components/main/MainPage";
+import Navbar from "../components/navbar/Navbar";
+import { getService } from "../json/jsonSlice";
+import { useAppDispatch } from "../store";
+import Login from "../components/Auth/Login";
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -28,11 +20,10 @@ function App(): JSX.Element {
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<MainPage />} />/
-        
         </Route>
+        <Route path="/admin" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-
     </div>
   );
 }
