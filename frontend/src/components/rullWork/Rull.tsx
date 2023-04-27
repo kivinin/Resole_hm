02143ebef27854314aabdfd78442d96cf082fa76
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-04-27 11:11:05 +0300

    fix css 2

--- frontend/src/components/rullWork/Rull.tsx
+++ frontend/src/components/rullWork/Rull.tsx
@@ -3,49 +3,49 @@ import './Rull.css';
 function Rull(): JSX.Element {
   return (
     <div className="borderRull">
-      <div style={{ fontSize: '60px', margin: '100px 0 100px 0',fontWeight:"800"}}>
-        Как мы работаем
+      <div style={{ fontSize: '55px', margin: '100px 0 100px 0',fontWeight:"600"}}>
+     КАК МЫ РАБОТАЕМ
       </div>
       <div className="stepper-wrapper">
         <div className="stepper-item completed">
           <div className="step-counter" style={{fontSize:"40px"}}>1</div>
           <img src="client.svg" className="imgStep" />
-          <div className="step-name" style={{ fontSize: '30px', color:"#b1fbe2", fontWeight:"900" }}>
-            Забираем
+          <div className="step-name" style={{ fontSize: '35px', color:"#1b9488", fontWeight:"900" }}>
+          ЗАБИРАЕМ
           </div>
-          <div className="step-name" style={{ width: '200px'}}>
-            Бесплатная доставка по городу Согласовываем с вами все работы после
-            осмотра изделий технологом
+          <div className="step-name" style={{ width: '350px', fontSize:"19px"}}>
+          БЕСПЛАТНАЯ ДОСТАВКА ПО ГОРОДУ СОГЛАСОВЫВАЕМ С ВАМИ ВСЕ РАБОТЫ ПОСЛЕ
+            ОСМОТРА ИЗДЕЛИЙ ТЕХНОЛОГОМ
           </div>
         </div>
         <div className="stepper-item completed" style={{display:"flex"}}>
           <div className="step-counter" style={{fontSize:"40px"}}>2</div>
           <img src="brush.svg" className="imgStep" />
-          <div className="step-name" style={{ fontSize: '30px', color:"#b1fbe2", fontWeight:"900" }}>
-          Восстанавливаем
+          <div className="step-name" style={{ fontSize: '35pxpx', color:"#1b9488", fontWeight:"900", margin:"0 0 0 -160px" }}>
+          ВОССТАНАВЛИВАЕМ
           </div>
-          <div className="step-name" style={{ width: '200px' }}>
-          Оплачиваете согласованные работы и мы приступаем к их выполнению, пока вы занимаетесь своими делами
+          <div className="step-name" style={{ width: '350px', fontSize:"19px" }}>
+          ОПЛАЧИВАЕТЕ СОГЛАСОВАННЫЕ РАБОТЫ И МЫ ПРИСТУПАЕМ К ИХ ВЫПОЛНЕНИЮ, ПОКА ВЫ ЗАНИМАЕТЕСЬ СВОИМИ ДЕЛАМИ
           </div>
         </div>
         <div className="stepper-item">
           <div className="step-counter" style={{fontSize:"40px"}}>3</div>
           <img src="delevery.svg" className="imgStep" />
-          <div className="step-name" style={{ fontSize: '30px', color:"#b1fbe2", fontWeight:"900" }}>
-          Привозим
+          <div className="step-name" style={{ fontSize: '35px', color:"#1b9488", fontWeight:"900" }}>
+          ПРИВОЗИМ
           </div>
-          <div className="step-name" style={{ width: '200px' }}>
-          Отправляем фото результата перед отправкой готового заказа и доставляем обратно
+          <div className="step-name" style={{ width: '350px', fontSize:"19px" }}>
+          ОТПРАВЛЯЕМ ФОТО РЕЗУЛЬТАТА ПЕРЕД ОТПРАВКОЙ ГОТОВОГО ЗАКАЗА И ДОСТАВЛЯЕМ ОБРАТНО
           </div>
         </div>
         <div className="stepper-item">
           <div className="step-counter" style={{fontSize:"40px"}}>4</div>
           <img src="application.svg" className="imgStep" />
-          <div className="step-name" style={{ fontSize: '30px', color:"#b1fbe2", fontWeight:"900" }}>
-          Гарантия
+          <div className="step-name" style={{ fontSize: '35px', color:"#1b9488", fontWeight:"900" }}>
+          ГАРАНТИЯ
           </div>
-          <div className="step-name" style={{ width: '200px' }}>
-          Оказываем постоянную online поддержку и не бросаем вас даже после выполненных работ
+          <div className="step-name" style={{ width: '350px', fontSize:"19px" }}>
+          ОКАЗЫВАЕМ ПОСТОЯННУЮ ONLINE ПОДДЕРЖКУ И НЕ БРОСАЕМ ВАС ДАЖЕ ПОСЛЕ ВЫПОЛНЕННЫХ РАБОТ
           </div>
         </div>
       </div>
