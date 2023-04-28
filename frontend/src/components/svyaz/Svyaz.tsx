bc8fb7228e1983a711b3d030e85e591cd07aab75
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-04-28 14:25:35 +0300

    final fix

--- frontend/src/components/svyaz/Svyaz.tsx
+++ frontend/src/components/svyaz/Svyaz.tsx
@@ -4,8 +4,8 @@ function Svyaz() {
   return (
     <div style={{display:"flex", height:"400px"}} className='conSv'>
       <div style={{display:"flex", flexDirection:"column", justifyContent:"space-evenly", width:"70%", alignItems:"center"}}>
-        <div style={{fontSize:"45px", color:"white", width:"60%", fontWeight:"600"}}>Получите бесплатную консультацию технолога</div>
-        <div style={{fontSize:"45px", color:"white", width:"60%", fontWeight:"600"}}>
+        <div style={{fontSize:"45px", color:"white", width:"65%", fontWeight:"600"}}>Получите бесплатную консультацию технолога</div>
+        <div style={{fontSize:"45px", color:"white", width:"65%", fontWeight:"600"}}>
           Заполните форму и наши специалисты свяжутся с вами в течение 30 минут!
         </div>
       </div>
